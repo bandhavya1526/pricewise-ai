e9de4e70748f3a90ac6692ad753989af2bb1cdfe
Bandhavya <bandhavya1526@gmail.com>
2026-04-27 20:40:24 +0530

    Fixed Render deployment port

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import pandas as pd
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -57,6 +57,8 @@ def recommend(id):
     })
 
 
+
 # Run Server
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
